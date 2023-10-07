a54faa8dbaad1503370d10397cba98da8c1912e5
SHwaier <59855335+SHwaier@users.noreply.github.com>
2023-10-07 19:36:03 -0400

    feat: add navbar

---
+++ src/components/AppNavbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
+function AppNavbar() {
+  return (
+    <Navbar expand="lg" className="navbar-container">
+      <Container>
+        <Navbar.Brand className="m-0" href="#home">
+          Blaze<span>Guard</span>.
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="hamburger-menu" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <p>Searchbar.</p>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#about" className="nav-links">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default AppNavbar;
